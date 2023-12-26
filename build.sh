3b08edf3cc119bf70125bb751939f5236d420b93
Lansong <2095621390@qq.com>
2023-12-26 21:10:02 +0800

    feat: 使用新的生成方法

--- build.sh
+++ build.sh
@@ -6,13 +6,8 @@ if [ $? != 0 ];then
 fi
 for file in $files
 do
-  proto_path=$(dirname "$file")
-    file_base=$(basename "$file" .proto)
-    goctl rpc protoc --proto_path=$proto_path $file --go_out=./go-zero-gen/$file_base --go-grpc_out=./go-zero-gen/$file_base --zrpc_out=./go-zero-gen/$file_base --style=goZero
-    rm -rf go-zero-gen/$file_base/etc
-    rm -rf go-zero-gen/$file_base/internal
-    rm -rf go-zero-gen/$file_base/$file_base.go
-    if [ $? != 0 ];then
-      exit 2
-    fi
+  kitex -module $module_name -I $idl_dir $file
+  if [ $? != 0 ];then
+    exit 2
+  fi
 done
